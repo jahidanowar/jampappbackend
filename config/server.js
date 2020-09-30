6209b79ac6f30b42d4ec3084e3020c22e45a3f56
jahidanowar <milon5143@gmail.com>
2020-09-30 11:08:03 +0530

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e033363855327e8940450b15c925b6da'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ac55b7e1-7d7a-43e7-97fc-ad9f71d22978'
+};
