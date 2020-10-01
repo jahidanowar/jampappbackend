c2212939492b8a74556c74a623df15918a64f3f6
jahidanowar <milon5143@gmail.com>
2020-10-01 22:35:05 +0530

    Database Change to work on boath Dev and Prod

--- config/database.js
+++ config/database.js
@@ -1,22 +1,43 @@
 const parse = require('pg-connection-string').parse;
 const config = parse(process.env.DATABASE_URL)
 
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'postgres',
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        username: config.user,
-        password: config.password,
+module.exports = ({ env }) => {
+
+  if(env('NODE_ENV') === 'development'){
+    return {
+      defaultConnection: 'default',
+      connections: {
+        default: {
+          connector: 'bookshelf',
+          settings: {
+            client: 'sqlite',
+            filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+          },
+          options: {
+            useNullAsDefault: true,
+          },
+        },
       },
-      options: {
-        ssl: false,
+    }
+  } else{
+    return{
+      defaultConnection: 'default',
+      connections: {
+        default: {
+          connector: 'bookshelf',
+          settings: {
+            client: 'postgres',
+            host: config.host,
+            port: config.port,
+            database: config.database,
+            username: config.user,
+            password: config.password,
+          },
+          options: {
+            ssl: false,
+          },
+        },
       },
-    },
-  },
-});
+    }
+  }
+};
