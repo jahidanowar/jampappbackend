a068cc42d7e542976f73433927790c872259f08e
Jahid Adnowar <milon5143@gmail.com>
2020-10-06 13:48:18 +0530

    Database Updated

--- config/database.js
+++ config/database.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-const parse = require('pg-connection-string').parse;
-const config = parse(process.env.DATABASE_URL ? process.env.DATABASE_URL : "");
-=======
   const parse = require('pg-connection-string').parse;
   const config = parse(process.env.DATABASE_URL ? process.env.DATABASE_URL : '');
->>>>>>> 89fb63e5b9102592a80015ade36301693e5fe224
 
 module.exports = ({ env }) => {
   if(env('NODE_ENV') === 'development'){
