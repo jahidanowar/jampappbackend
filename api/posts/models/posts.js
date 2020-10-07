87dc9c304e776b0386a6ec5a60d558df09ed4531
Jahid Adnowar <milon5143@gmail.com>
2020-10-07 11:41:31 +0530

    Slug Functionality Added

--- api/posts/models/posts.js
+++ api/posts/models/posts.js
@@ -1,8 +1,30 @@
-'use strict';
+const slugify = require("slugify");
+
+("use strict");
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+
+  lifecycles: {
+    //Create Slug Based on Title if slug exist then skip
+    async beforeCreate(data) {
+      if(!data.slug && data.Title){
+        data.slug = slugify(data.Title, { lower: true });
+      }else{
+        data.slug = data.slug
+      }
+    },
+    async beforeUpdate(params, data) {
+      if(!data.slug && data.Title){
+        data.slug = slugify(data.Title, { lower: true });
+      }else{
+        data.slug = data.slug
+      }
+    },
+    //SLug Operation Ends
+  },
+};
